2dcb8f37925f2dcb9ed35ffc1fc7764f7c1e84a6
romanshusterman <romanshusterman@gmail.com>
2023-01-07 18:39:40 +0200

    Create application.py

---
+++ application.py
@@ -0,0 +1,13 @@
+#application.py
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Optionsoptions = Options()
+options.add_argument("--headless")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com/")
+element_text = driver.page_source
+print(element_text)
